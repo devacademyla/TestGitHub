318f3d8df782cf1fc22c7a98ddf225492bf03cc1
Moisés Ñañez <moisesnandres@gmail.com>
2017-05-16 13:24:19 -0500

    Add label test api

--- add_milestone.rb
+++ add_milestone.rb
@@ -2,5 +2,6 @@ require 'github_api'
 require 'dotenv/load'
 
 issues = Github::Client::Issues.new oauth_token: ENV['ACCESS_TOKEN'], user: 'devacademyla', repo: 'caregivers-ionic'
-issues.labels.create name: 'api-test', color: '333'
-# puts issues.labels.list
+issues.milestones.create title: 'hello-world',
+  state: 'open',
+  description: 'Milestone created using github_api'
