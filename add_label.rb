29878e14d05cb1e1062aa61eb7dd10ea485c3e1c
Moisés Ñañez <moisesnandres@gmail.com>
2017-05-16 13:41:35 -0500

    Add label to issue/PR

--- add_label.rb
+++ add_label.rb
@@ -1,8 +1,14 @@
 require 'github_api'
 require 'dotenv/load'
 
-issues = Github::Client::Issues.new oauth_token: ENV['ACCESS_TOKEN'], user: 'devacademyla', repo: 'caregivers-ionic'
+# Create label
+# issues = Github::Client::Issues.new oauth_token: ENV['ACCESS_TOKEN'], user: 'devacademyla', repo: 'caregivers-ionic'
 # issues.labels.create name: 'api-test', color: '333'
 # puts issues.labels.list
-issues.labels.update 'devacademyla', 'caregivers-ionic', 'duplicate', name: 're duplicate', color: '666'
+# issues.labels.update 'devacademyla', 'caregivers-ionic', 'duplicate', name: 're duplicate', color: '666'
 # puts issues.labels.list
+
+
+# Add label to PR
+github = Github.new oauth_token: ENV['ACCESS_TOKEN']
+github.issues.labels.add 'devacademyla', 'caregivers-ionic', '1', 'api-test'
