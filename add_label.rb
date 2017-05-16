8e7bdf588f04d61de216c33232c7df2447b8564e
Moisés Ñañez <moisesnandres@gmail.com>
2017-05-16 13:28:17 -0500

    Update label name and color

--- add_label.rb
+++ add_label.rb
@@ -2,5 +2,7 @@ require 'github_api'
 require 'dotenv/load'
 
 issues = Github::Client::Issues.new oauth_token: ENV['ACCESS_TOKEN'], user: 'devacademyla', repo: 'caregivers-ionic'
-issues.labels.create name: 'api-test', color: '333'
+# issues.labels.create name: 'api-test', color: '333'
+# puts issues.labels.list
+issues.labels.update 'devacademyla', 'caregivers-ionic', 'duplicate', name: 're duplicate', color: '666'
 # puts issues.labels.list
